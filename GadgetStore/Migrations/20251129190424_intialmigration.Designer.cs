c917caeb1f0424c8f1f20599ea8fb17c592a4d2a
zouloulou75 <aloulouzamouri8@gmail.com>
2025-11-29 20:24:10 +0100

    Initial commit: Full GadgetStore e-commerce project (.NET + workflows)

--- GadgetStore/Migrations/20251129190424_intialmigration.Designer.cs
+++ GadgetStore/Migrations/20251129190424_intialmigration.Designer.cs
@@ -4,6 +4,7 @@ using GadgetStore.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GadgetStore.Migrations
 {
     [DbContext(typeof(GadgetStoreDbContext))]
-    partial class GadgetStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251129190424_intialmigration")]
+    partial class intialmigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
